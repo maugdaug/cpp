69925ec8d5ab1a9e7219a66722ce43495f8572f7
root <root@DESKTOP-79GJGQL.localdomain>
2022-10-23 22:10:57 -0700

    started Learning C++

---
+++ 1-Learning_C++/01_04-console_interaction/CodeDemo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter your name: ";
+    cin >> str;
+    cout << "Hello, " << str << "!" << endl; return(0);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+This repo correlates to the "Advance your skills in C++" set of courses on Linkedin Learning:
+https://www.linkedin.com/learning/paths/advance-your-skills-in-c-plus-plus
