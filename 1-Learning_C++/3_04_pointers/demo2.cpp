c0b0c11f30ee4b687140341a7881506fb4bfbbd2
root <root@DESKTOP-79GJGQL.localdomain>
2022-11-03 15:53:15 -0700

    pointers

---
+++ 1-Learning_C++/3_04_pointers/demo2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include "cow.h"
+
+using namespace std;
+
+int a = 37;
+int *ptr;
+
+cow *my_cow;
+
+int main() {
+	ptr = &a;
+
+	cout << "           The content of a is " << a << endl;
+	cout << "    ptr is pointing to address " << ptr << endl;
+	cout << "           The address of a is " << &a << endl;
+	cout << "Where ptr is pointing, we have " << *ptr << endl;
+	cout << "         The address of ptr is " << &ptr << endl;
+	cout << endl;
+
+	// cow my_cow("Hank", 7, pet);
+	my_cow = new cow("Howard", 3, hide);
+
+	//my_cow.age = 5;
+	//my_cow.name = "Gary";
+	//my_cow.purpose = meat;
+
+	cout << my_cow->get_name() << " is a type " << (int)my_cow->get_purpose() << " cow." << endl;
+	cout << my_cow->get_name() << " is " << my_cow->get_age() << " years old." << endl;
+
+	delete my_cow; // opposite of "new"
+	
+	return (0);
+}
